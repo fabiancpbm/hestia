8581b1a2283a1fdbf6d43e470cc1bd709bd88384
Fabian Brandão <fabiancpbm@gmail.com>
2022-07-07 00:28:26 -0300

    Adding getters and setters.

--- metamodel/src/main/java/Assignment.java
+++ metamodel/src/main/java/Assignment.java
@@ -1,3 +1,12 @@
 public class Assignment extends FunctionCall {
     private Value variable;
+
+    public Value getVariable() {
+        return variable;
+    }
+
+    public void setVariable(Value variable) {
+        this.variable = variable;
+    }
+
 }
