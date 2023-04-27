d89bc18bf1011d6c16db039bf5c292bb67cc1d48
Fabian Brandão <fabian.brandao@nubank.com.br>
2023-04-27 10:01:12 -0300

    removing the need of using a json file. fixing test tool

--- src/test/java/br/com/chongroup/hestia/parser/ParserTest.java
+++ src/test/java/br/com/chongroup/hestia/parser/ParserTest.java
@@ -4,15 +4,12 @@ import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ParserTest {
 
     @Test
     public void helloTestFunction() throws IOException, MavenInvocationException {
-        ParserTestMetadata parserTestMetadata = ParserTestTool.readParserTestMetadata();
-        ParserTestTool.runParser(parserTestMetadata);
-        Assert.assertTrue(ParserTestTool.match(parserTestMetadata));
+        Assert.assertTrue(ParserTestTool.runTest());
     }
 }
--- src/test/java/br/com/chongroup/hestia/parser/ParserTestTool.java
+++ src/test/java/br/com/chongroup/hestia/parser/ParserTestTool.java
@@ -1,82 +1,90 @@
 package br.com.chongroup.hestia.parser;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
 import org.apache.maven.shared.invoker.*;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Properties;
 
 public class ParserTestTool {
 
     private static final String RESOURCES_PATH = "src/test/resources";
-    private static final String THING_PATH = "src/test/thing";
+    private static final String TARGET_PATH = "target";
 
-    public static ParserTestMetadata readParserTestMetadata() throws FileNotFoundException {
+    public static boolean runTest() throws MavenInvocationException, IOException {
         StackTraceElement[] stackTrace = new Throwable().getStackTrace();
         StackTraceElement stackTraceElement = stackTrace[1];
+        String classPath = stackTraceElement.getClassName().replaceAll("\\.", File.separator);
         String methodName = stackTraceElement.getMethodName();
-        String jsonFileName = stackTraceElement.getClassName().replaceAll("\\.", File.separator) + ".json";
 
-        Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader(
-                System.getProperty("user.dir") +
-                        File.separator +
-                        RESOURCES_PATH +
-                        File.separator +
-                        jsonFileName));
-        List<ParserTestMetadata> parserTestMetadataList = gson.fromJson(reader, new TypeToken<ArrayList<ParserTestMetadata>>(){}.getType());
-        ParserTestMetadata parserTestMetadata = parserTestMetadataList.stream().filter(e -> methodName.equals(e.getTestName())).findAny().orElse(null);
-        return parserTestMetadata;
+        generateParserJar();
+        runParserJar(classPath, methodName);
+
+        String expectedPath = getAbsolutePath(RESOURCES_PATH, classPath, methodName, "expected");
+        String actualPath = getAbsolutePath("projetinho", "Project");
+        return match(expectedPath, actualPath);
     }
 
-    public static void runParser(ParserTestMetadata parserTestMetadata) throws MavenInvocationException, IOException {
+    private static boolean match(String expectedPath, String actualPath) {
+        File expectedFile = new File(expectedPath);
+        if (expectedFile.isFile()) {
+            String actualFilePath = actualPath + File.separator + expectedFile.getAbsolutePath().substring(expectedPath.length());
+            String actualContent = readFile(actualFilePath);
+
+            String expectedContent = readFile(expectedFile.getAbsolutePath());
+
+            System.out.println(expectedContent);
+            System.err.println("=====");
+            System.out.println(actualContent);
+            return false;
+        }
+
+        for (File file : expectedFile.listFiles()) {
+            if (!match(file.getAbsolutePath(), actualPath)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String readFile(String filePath) {
+        try {
+            BufferedReader  reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append(ls);
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static void generateParserJar() throws MavenInvocationException {
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile(new File(System.getProperty("user.dir") + File.separator + "pom.xml"));
         request.setGoals(Arrays.asList("compile", "assembly:single"));
         Invoker invoker = new DefaultInvoker();
         invoker.execute(request);
-
-        Runtime runtime = Runtime.getRuntime();
-        runtime.exec("java -jar " + System.getProperty("user.dir") + File.separator + "target/hestia-1.0-SNAPSHOT-jar-with-dependencies.jar" + " " + System.getProperty("user.dir") + File.separator + THING_PATH + File.separator + parserTestMetadata.getThingSource());
-//        request.setGoals(Arrays.asList("build-helper:add-source"));
-//        invoker = new DefaultInvoker();
-//        invoker.execute( request );
-//
-//        request.setGoals(Arrays.asList("assembly:single"));
-//        invoker = new DefaultInvoker();
-//        invoker.execute( request );
-//        if (nao existe java) {
-//            criar java
-//        }
-//        if (nao existe jar) {
-//            cria jar
-//        }
-//
-//        executar jar
-//        new ThingComp(new java.io.FileInputStream(
-//                System.getProperty("user.dir") +
-//                        File.separator +
-//                        THING_PATH +
-//                        File.separator +
-//                        parserTestMetadata.getThingSource()));
-//        ThingComp.ThingBody();
     }
 
-    public static boolean match(ParserTestMetadata parserTestMetadata) {
-//        for (String expected : parserTestMetadata.getExpected()) {
-//            File file = new File(System.getProperty("user.dir") +
-//                    File.separator +
-//                    "projetinho" + File.separator + "Project" + File.separator +
-//                    expected);
-//            if (!file.exists()) {
-//                return false;
-//            }
-//        }
-        return true;
+    private static void runParserJar(String classPath, String methodName) throws IOException {
+        Properties properties = new Properties();
+        properties.load(ParserTestTool.class.getClassLoader().getResourceAsStream("pom.properties"));
+        String jarName = properties.getProperty("artifact") + "-" + properties.getProperty("version") + "-jar-with-dependencies" + ".jar";
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("java -jar " +  getAbsolutePath(TARGET_PATH, jarName) + " " + getAbsolutePath(RESOURCES_PATH, classPath, methodName + ".thing"));
+    }
+
+    private static String getAbsolutePath(String... relativePath) {
+        StringBuilder absolutePath = new StringBuilder(System.getProperty("user.dir"));
+        for (String path : relativePath) {
+            absolutePath.append(File.separator).append(path);
+        }
+        return absolutePath.toString();
     }
 }
