699c71bf2eb1436153156ecb53e6411ff231a099
Fabian Brandão <fabian.brandao@nubank.com.br>
2023-04-27 18:48:04 -0300

    finishing the fix for parser test scenarios

--- src/test/java/br/com/chongroup/hestia/parser/ParserTestTool.java
+++ src/test/java/br/com/chongroup/hestia/parser/ParserTestTool.java
@@ -5,9 +5,11 @@ import org.apache.maven.shared.invoker.*;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class ParserTestTool {
 
+    private static final Logger LOGGER = Logger.getLogger(ParserTestTool.class.getSimpleName());
     private static final String RESOURCES_PATH = "src/test/resources";
     private static final String TARGET_PATH = "target";
 
@@ -28,22 +30,20 @@ public class ParserTestTool {
     private static boolean match(String expectedPath, String actualPath) {
         File expectedFile = new File(expectedPath);
         if (expectedFile.isFile()) {
-            String actualFilePath = actualPath + File.separator + expectedFile.getAbsolutePath().substring(expectedPath.length());
+            String actualFilePath = actualPath + File.separator + expectedFile.getAbsolutePath().split("/expected/")[1];
             String actualContent = readFile(actualFilePath);
-
             String expectedContent = readFile(expectedFile.getAbsolutePath());
-
-            System.out.println(expectedContent);
-            System.err.println("=====");
-            System.out.println(actualContent);
-            return false;
+            LOGGER.fine("Asserting the expected file " + expectedPath + " with actual file " + actualFilePath);
+            return expectedContent != null && expectedContent.equals(actualContent);
         }
 
         for (File file : expectedFile.listFiles()) {
             if (!match(file.getAbsolutePath(), actualPath)) {
+                LOGGER.severe("The file " + actualPath + " does not match with the file " + expectedPath);
                 return false;
             }
         }
+
         return true;
     }
 
@@ -77,7 +77,7 @@ public class ParserTestTool {
         properties.load(ParserTestTool.class.getClassLoader().getResourceAsStream("pom.properties"));
         String jarName = properties.getProperty("artifact") + "-" + properties.getProperty("version") + "-jar-with-dependencies" + ".jar";
         Runtime runtime = Runtime.getRuntime();
-        runtime.exec("java -jar " +  getAbsolutePath(TARGET_PATH, jarName) + " " + getAbsolutePath(RESOURCES_PATH, classPath, methodName + ".thing"));
+        runtime.exec("java -jar " +  getAbsolutePath(TARGET_PATH, jarName) + " " + getAbsolutePath(RESOURCES_PATH, classPath, methodName, methodName + ".thing"));
     }
 
     private static String getAbsolutePath(String... relativePath) {
