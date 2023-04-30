c117d695dac7a60b48e899a2913fbd4e8e86f797
Fabian Brandão <fabian.brandao@nubank.com.br>
2023-04-30 00:23:13 -0300

    partial - simple hardware code sintax and expected result

---
+++ src/test/resources/br/com/chongroup/hestia/parser/StructuralParserTest/expected/Hardware/simple_hardware.ino
@@ -0,0 +1,20 @@
+#define VALVE__DATA_PIN 13;
+
+void setup() {
+    pinMode(VALVE__DATA_PIN, OUTPUT);
+}
+
+void sprinkler__status() {}
+
+void sprinkler__on() {}
+
+void sprinkler__off() {}
+
+void open() {}
+
+void close() {}
+
+void status() {}
+
+void loop() {
+}
