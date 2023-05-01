fe4098476c6d0a10a91823a6778cbf31c7c8a11d
Fabian Brandão <fabian.brandao@nubank.com.br>
2023-04-30 22:55:58 -0300

    adding all expected file in simple hardware test

---
+++ src/test/resources/br/com/chongroup/hestia/parser/StructuralParserTest/expected/Hardware/simple_hardware/sprinkler.ino
@@ -0,0 +1,5 @@
+void sprinkler__status() {}
+
+void sprinkler__on() {}
+
+void sprinkler__off() {}
---
+++ src/test/resources/br/com/chongroup/hestia/parser/StructuralParserTest/expected/Hardware/simple_hardware/valve.ino
@@ -0,0 +1,7 @@
+#define VALVE__ACT_PIN 13;
+
+void valve__open() {}
+
+void valve__close() {}
+
+void valve__status() {}
